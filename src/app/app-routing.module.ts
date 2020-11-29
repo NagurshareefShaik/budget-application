e8073024446ce3da993846cc2adee0e5103cd1e9
Nagurshareef <nagurshareef123@gmail.com>
2020-11-29 12:46:32 +0530

    initial ui commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'transactions',component:TransactionsComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'accounts',component:AccountsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
